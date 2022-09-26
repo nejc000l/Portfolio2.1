4b6f07f961f736dcaab0195d8ad3b8199e387a28
nejc000l <nejc.lampic@gmail.com>
2022-09-26 20:31:21 +0200

    f

--- sanity.ts
+++ sanity.ts
@@ -5,7 +5,6 @@ export const config = {
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
     apiVersion:'2022-09-26',
-    token: process.env.SANITY_API_TOKEN,
     useCdn:process.env.NODE_ENV === 'production',
 
 }
