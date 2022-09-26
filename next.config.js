b07966018b24c3ef96aa320ba6cf488e1179a082
nejc000l <nejc.lampic@gmail.com>
2022-09-27 00:04:30 +0200

    1

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    NEXT_PUBLIC_BASE_URL: "http://localhost:3000",
-    NEXT_PUBLIC_SANITY_PROJECT_ID: "uw3n4vcz",
-    NEXT_PUBLIC_SANITY_DATASET:"production",
+    NEXT_PUBLIC_BASE_URL: http://localhost:3000",
+    NEXT_PUBLIC_SANITY_PROJECT_ID: uw3n4vcz,
+    NEXT_PUBLIC_SANITY_DATASET:production,
   },
 
 }
