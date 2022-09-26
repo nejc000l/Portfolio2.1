23584f3cf50f91b3a44eea2f25d0af472c2b9a20
nejc000l <nejc.lampic@gmail.com>
2022-09-26 20:51:58 +0200

    f

--- utils/fetchExperiences.ts
+++ utils/fetchExperiences.ts
@@ -1,10 +1,9 @@
 import {Experience} from '../typings'
 export const fetchExperiences = async () =>{
-    let data=[]
+    let data
     const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getExperience`)
      data = await res.json()
-    data = JSON.parse(JSON.stringify(data))
-    const experience:Experience[]= data.experience;
-    console.log(data)
+    const experience:Experience[]= JSON.parse(JSON.stringify(data.experience));
+    
     return experience
 }
