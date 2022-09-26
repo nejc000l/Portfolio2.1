9743efbe016fea0cf9264016b1e93b82e45d6d84
nejc000l <nejc.lampic@gmail.com>
2022-09-27 00:24:13 +0200

    1

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  env: {
-    NEXT_PUBLIC_BASE_URL: "https://portfolio2-1-wn3v.vercel.app",
-    NEXT_PUBLIC_SANITY_PROJECT_ID: "uw3n4vcz",
-    NEXT_PUBLIC_SANITY_DATASET:"production",
-  },
+
 
 }
